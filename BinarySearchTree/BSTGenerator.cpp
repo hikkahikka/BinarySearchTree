88ff40b12435c7483ef70613c3cf73bc4cbeed3e
Boris <mr_boriska1998@mail.ru>
2025-06-11 12:10:17 +0300

    ReworkBSTGenerator

--- BinarySearchTree/BSTGenerator.cpp
+++ BinarySearchTree/BSTGenerator.cpp
@@ -1,17 +1,7 @@
 #include "BSTGenerator.h"
 void BSTGenerator::GenerateTree(BST& tree, int n, int start, int finish) {
 	tree.DeleteTree();
-	int value;
-	for (int i = 0; i< n; i++) {
-		value = rand() % (finish - start + 1);
-		if (!tree.ContainsNode(value)) {
-			tree.AddNode(value);
-
-		}
-		else {
-			i--;
-		}
-	}
+	GenerateNodes(tree, n, start, finish);
 }
 
 void BSTGenerator::GenerateTree(BST& tree, int n) {
@@ -35,3 +25,4 @@ void BSTGenerator::GenerateNodes(BST& tree, int n, int start, int finish) {
 void BSTGenerator::GenerateNodes(BST& tree, int n) {
 	GenerateNodes(tree, n, 0, 100);
 }
+
--- BinarySearchTree/BSTGenerator.h
+++ BinarySearchTree/BSTGenerator.h
@@ -7,9 +7,7 @@ public:
 
 	void GenerateTree(BST& tree, int n, int start, int finish);
 	void GenerateTree(BST& tree, int n);
-
 	void GenerateNodes(BST& tree, int n, int start, int finish);
 	void GenerateNodes(BST& tree, int n);
-
 };
 
